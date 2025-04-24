ffb0c905c127666db8b98e7fa0b64c32cf7dcb18
刘越 <3379850961@qq.com>
2025-04-24 22:29:44 +0800

    优化仓库

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -13,7 +13,58 @@ request.interceptors.response.use(
   (response) => { //成功回调
     return response.data
   },
-  (error) => { //失败回调
+  (error) => { //失败回调                                \\  0\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if (error.response.status === 401) {
       // 提示信息
       ElMessage.error("登录超时，请重新登录");

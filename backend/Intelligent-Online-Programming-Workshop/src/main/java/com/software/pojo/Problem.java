b725a9fd41884fa9f105ee3741192b8309c5a078
刘越 <3379850961@qq.com>
2025-04-24 08:52:48 +0800

    新建Problem类

---
+++ backend/Intelligent-Online-Programming-Workshop/src/main/java/com/software/pojo/Problem.java
@@ -0,0 +1,28 @@
+package com.software.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Problem {
+    private Integer id;
+    private String title;
+    private String description;
+    private String inputExample;
+    private String outputExample;
+    private int timeLimit;
+    private int memoryLimit;
+    private int difficulty;
+    private String tags;
+    private double passRate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}

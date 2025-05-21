60ebd1c33ae052c0e95f0ea8eb54887916387b84
刘越 <3379850961@qq.com>
2025-05-21 20:01:50 +0800

    整体格式优化

--- CodeMind-Web/src/api/login.js
+++ CodeMind-Web/src/api/login.js
@@ -1,4 +1,4 @@
 import request from '@/utils/request';
 
 // 登录接口
-export const loginApi = (data) => request.post('/users/login',data);
+export const loginApi = (data) => request.post('/user/user/login',data);
--- CodeMind-Web/src/api/problem.js
+++ CodeMind-Web/src/api/problem.js
@@ -3,11 +3,11 @@ import request from '@/utils/request';
 // 查询题库
 export const queryPageApi = (title,difficulty,tags,page,pageSize) => {
     const tagString = tags.join(',');
-    return request.get(`/problems?title=${title}&difficulty=${difficulty}&tags=${tagString}&page=${page}&pageSize=${pageSize}`);
+    return request.get(`/user/problems?title=${title}&difficulty=${difficulty}&tags=${tagString}&page=${page}&pageSize=${pageSize}`);
 }
 
 // 查询标签库
-export const queryTagsApi = () => request.get('/problems/tags');
+export const queryTagsApi = () => request.get('/user/problems/tags');
 
 // 通过id查询题目详情
 export const queryProblemByIdApi = (id) => request.get(`/problems/${id}`);
